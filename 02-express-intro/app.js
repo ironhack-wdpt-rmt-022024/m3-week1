9a9b4ce16dd1b770c05c92a02dcc1c7747236339
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2024-06-29 14:25:33 +0200

    add lesson code - m3 w1 day 3

--- 02-express-intro/app.js
+++ 02-express-intro/app.js
@@ -1,28 +1,44 @@
 const express = require("express");
 const morgan = require("morgan");
+
 // Instantiate the server
 const app = express();
 
-// Middleware functions - syntax/pattern
-// 1. Use built-in middleware from Express. Example: express.static, ...
-// 2. Install npm middleware package: morgan
-// 3. Create your own middleware
+/* Middleware functions - syntax/pattern
+1. Use built-in middleware from Express. Example: express.static, express.json ...
+2. Install npm middleware package: morgan
+3. Create your own middleware
+
+*/
 
 // SETUP MIDDLEWARE: Always set up your middleware before the routes
 // Logging middleware
 app.use(morgan("dev"));
+
 // Handle static files
 app.use(express.static("public"));
+// Handle parsing of JSON data on requests
+app.use(express.json());
 
-// Create Routes
-app.get("/", (req, res, next) => {
-  res.sendFile(__dirname + "/views/index.html");
+// Custom middleware
+app.use(function (req, res, next) {
+  console.log("HELLO FROM MY MIDDLEWARE!");
+  next();
 });
 
+// ROUTES
+//  GET '/'
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/views/index.html");
+  next();
+});
+
+//  GET '/about'
 app.get("/about", (req, res) => {
   res.sendFile(__dirname + "/views/about.html");
 });
 
+//  GET '/example'
 app.get("/example", (req, res) => {
   const data = {
     firstName: "Sarah",
